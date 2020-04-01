4b542a8f410d8ee38dd14c8d9e67dd61bb21f3a8
Sylwek Piorko <termetal@gmail.com>
2020-04-01 08:21:01 +0100

    Initial commit

---
+++ boards/tests/test_view_home.py
@@ -0,0 +1,26 @@
+from django.urls import reverse, resolve
+
+from django.test import TestCase
+
+from ..views import BoardListView
+from ..models import Board
+
+
+class HomeTests(TestCase):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django Board.')
+        url = reverse('home')
+        self.response = self.client.get(url)
+        
+    def test_home_view_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        # self.assertEqual(view.func, home)
+        self.assertEqual(view.func.view_class, BoardListView)
+
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        # print(self.response.content)
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
